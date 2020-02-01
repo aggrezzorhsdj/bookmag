5719b30f7f81cd8dfe58dc68043d77727332b781
aggrezzorhsdj <aggrezzorhsdj@gmail.com>
2020-02-01 13:40:16 +0300

    update project

---
+++ src/app/books.interface.ts
@@ -0,0 +1,11 @@
+export interface BooksInterface {
+  id: number;
+  author: string;
+  category: string;
+  published: Date;
+  title: string;
+  desc: string;
+  price: number;
+  old_price: number;
+  left: number;
+}
