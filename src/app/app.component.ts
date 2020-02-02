c128acf8c202037c910f044d9a8f86e1dbf8383f
aggrezzorhsdj <aggrezzorhsdj@gmail.com>
2020-02-02 22:36:12 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'bookmag';
+export class AppComponent implements OnInit{
+  title = 'Bookmag';
+  constructor() {}
+  ngOnInit() {}
 }
