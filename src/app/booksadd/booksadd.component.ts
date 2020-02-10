4cee06c9139a4332be1ad9f98645ff48cc12412e
aggrezzorhsdj <aggrezzorhsdj@gmail.com>
2020-02-10 23:02:37 +0300

    update with components

---
+++ src/app/booksadd/booksadd.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-booksadd',
+  templateUrl: './booksadd.component.html',
+  styleUrls: ['./booksadd.component.less']
+})
+export class BooksaddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
