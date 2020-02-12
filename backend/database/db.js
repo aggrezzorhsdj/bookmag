d5898f8613bd4af5955c5aa38d43889be26437d0
aggrezzorhsdj <aggrezzorhsdj@gmail.com>
2020-02-12 17:30:39 +0300

    update

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-  db: 'mongodb://localhost:27017/bookmagdb'
+  db: 'mongodb://localhost:27017/bookmagdb',
+  dbremote: 'mongodb+srv://test:test>@cluster0-jazwb.mongodb.net/test?retryWrites=true&w=majority/bookmagdb'
 };
